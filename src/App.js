c09d55defaf1606465667ace2fff2d1e243fefcc
SaketR3 <saketr3@illinois.edu>
2024-01-12 16:26:24 -0600

    Finished web app 1.0

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import LandingPage from './components/LandingPage';
+import GuideCreationPage from './components/GuideCreationPage'; 
+
+export default function App() {
+    return (<>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/create" element={<GuideCreationPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </>)
+}
